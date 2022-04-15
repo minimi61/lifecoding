69fccede52bcf16a231b01d611dd20530c817d66
minimi61 <new1573@naver.com>
2022-04-15 13:15:15 +0900

    secod commit State

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
 function Header(props) {
   console.log('props',props)
   return <header>
@@ -16,7 +19,7 @@ function Nav(props) {
     lis.push(<li key={t.id}>
       <a id={t.id} href={'/read' + t.id} onClick={event => {
         event.preventDefault();
-        props.onChangeMode(event.target.id);
+        props.onChangeMode(Number(event.target.id));
        }}>{t.title}</a>
     </li>)
   }
@@ -36,19 +39,37 @@ function Article(props) {
       </articles>
 }
 function App() {
+  const [mode, setMode] = useState('Welcome');
+  const [id, setId] = useState(null);
   const topics = [
         {id:1, title:'html', body:'html is ...'},
         {id:2, title:'css', body:'css is ...'},
         {id:3, title:'js', body:'js is ...'}
   ]
-  
+  let content = null;
+  if (mode === 'Welcome') {
+    content=  <Article title="Welcome" body="Hello, WEB" onChangeMode={(id)=>{alert('lala')}}></Article>;
+  } else if (mode === "Read") {
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++){
+      console.log('오잉',topics[i].id, id);
+      if ((topics[i].id) === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }    
+    }
+    content= <Article title={title} body={body} onChangeMode={(id)=>{alert('lala')}}></Article>
+  }
   return (
     <div>
       <Header title="REACT" onChangeMode={ () => {
-        alert('Header')
+        setMode('Welcome');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id) => {alert(id)}}></Nav>
-      <Article title="Welcome" body="Hello, WEB" onChangeMode={(id)=>{alert('lala')}}></Article>
+      <Nav topics={topics} onChangeMode={(_id) => {
+        setMode('Read');
+        setId(_id);
+      }}></Nav>
+      { content }
     </div>
   )
 }
